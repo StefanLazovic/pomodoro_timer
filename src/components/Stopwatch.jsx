553a76b379aa1577550d980b1aff93bdfc482788
Stefan <stefan.lazovic@growthmill.com>
2022-03-29 14:17:44 +0200

    seconds and minutes are merged into a single state object, minutes counting and pause counting is added, stopwatch time is temporarily shortened for faster-checking behaviour of the stopwatch

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,35 +1,64 @@
 import React, { useEffect, useState } from 'react'
 
 const Stopwatch = () => {
-	const [seconds, setSeconds] = useState(0)
-	const [starter, setStarter] = useState(false)
+	const [disabled, setDisabled] = useState(false)
+	const [countdown, setCountdown] = useState({ minutes: 5, seconds: 0, triggerFiveMin: false })
 
 	useEffect(() => {
-		if (seconds === 59) {
-			setInterval(() => {
-				setSeconds(prevState => {
-					if (prevState === 0) {
-						return 59
+		if (countdown.seconds === 1) {
+				const interval = setInterval(() => {
+				setCountdown(prevState => {
+					if (prevState.seconds === 0) {
+						if (prevState.minutes === 0 && prevState.triggerFiveMin === false) {
+							return {
+								...prevState,
+								minutes: 3,
+								seconds: 1,
+								triggerFiveMin: true
+							}
+						}
+						if (prevState.minutes === 0 && prevState.seconds === 0 && prevState.triggerFiveMin === true) {
+							setDisabled(false)
+							clearInterval(interval)
+							return {
+								...prevState,
+								minutes: 5,
+								seconds: 0,
+								triggerFiveMin: false
+							}
+						}
+						return {
+							...prevState,
+							minutes: prevState.minutes - 1,
+							seconds: 1
+						}
 					}
-					return prevState - 1
+					return {
+						...prevState,
+						seconds: prevState.seconds - 1
+					}
 				})
 			}, 1000)
 		}
-	}, [starter])
+	}, [disabled])
 	
 	const start = () => {
-		setSeconds(59)
-		setStarter(true)
+		setCountdown({
+			minutes: 5,
+			seconds: 1,
+			triggerFiveMin: false
+		})
+		setDisabled(true)
 	}
 
 	return (
 		<>
 			<br />
-			{seconds < 10 && 0}{seconds}
+			{countdown.minutes < 10 && 0}{countdown.minutes}:{countdown.seconds < 10 && 0}{countdown.seconds}
 			<br />
 			<button 
 				onClick={start} 
-				disabled={starter}
+				disabled={disabled}
 			>
 				Start
 			</button>
